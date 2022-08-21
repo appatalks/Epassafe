511a06bc4c310d48e2d350b0dd0fab63ead9cdae
servingbaby <charon_isis@hotmail.com>
2022-08-21 13:28:20 -0500

    play store prep

--- app/build/generated/source/buildConfig/debug/com/epassafe/upm/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/epassafe/upm/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.epassafe.upm";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 29;
-  public static final String VERSION_NAME = "2.9";
+  public static final int VERSION_CODE = 30;
+  public static final String VERSION_NAME = "3.0";
 }
